2b659a5bc3e6e88228a6dcf997316e194191097b
dabben1993 <gickis1993@live.com>
2024-01-29 11:41:16 +0100

    refactored file_helper.py to implement exception handling

--- src/file_helper.py
+++ src/file_helper.py
@@ -1,32 +1,31 @@
 import json
 import os
 import yaml
-import structlog
+from structlog import get_logger
 
-log = structlog.get_logger()
+log = get_logger()
 
 
 class FileHandler:
-    def __init__(self, source_directory, destination_directory):
-        self.source_directory = source_directory
-        self.destination_directory = destination_directory
+    def __init__(self, file_path, destination):
+        self.file_path = file_path
+        self.destination = destination
 
-    def read_yaml(self, file_name):
-        with open(file_name, 'r') as file:
-            log.info("File opened", file=file_name + " opened")
-            return yaml.safe_load(file)
+    def read_yaml(self):
+        try:
+            with open(self.file_path, 'r') as yaml_file:
+                return yaml.safe_load(yaml_file)
+        except Exception as e:
+            log.error("Error reading YAML file", error=str(e))
+            return None
 
-    def convert_to_json(self, data):
-        log.info("File to be converted", org_file=data)
-        json_data = data.to_json()
-        log.info("File converted to json", json_output=json_data)
-        return json_data
-
-    def save_as_json(self, data, output_directory, file_name):
-        json_output_path = os.path.join(output_directory, file_name + ".json")
-        os.makedirs(output_directory, exist_ok=True)
-
-        with open(json_output_path, 'w') as json_output_file:
-            json_output_file.write(data)
-
-        log.info("JSON object created", path=json_output_path)
+    def save_as_json(self, data, file_name, output_directory=None):
+        output_directory = output_directory or self.destination
+        try:
+            os.makedirs(output_directory, exist_ok=True)
+            json_output_path = os.path.join(output_directory, file_name + ".json")
+            with open(json_output_path, 'w') as json_output_file:
+                json.dump(data, json_output_file, indent=2)
+            log.info("File saved as JSON", data=data, file_path=json_output_path)
+        except Exception as e:
+            log.error("Error saving JSON file", error=str(e))
