42e095ddb23a405d9c5a1ed2c43b15fe1911219f
dabben1993 <gickis1993@live.com>
2024-01-29 11:40:46 +0100

    refactored config_validator.py

--- src/config_validator.py
+++ src/config_validator.py
@@ -16,6 +16,7 @@ class ConfigValidator:
         self.json_output_path = json_output_path
         self.destination = destination
         self.validator = self.load_schema()
+        self.file_handler = FileHandler(self.file_path, self.destination)
         self.process_configuration()
 
     def load_schema(self):
@@ -25,28 +26,24 @@ class ConfigValidator:
             return cerberus.Validator(schema)
 
     def process_configuration(self):
-        file_handler = FileHandler(self.file_path, self.destination)
-        config_data = file_handler.read_yaml(self.file_path)
+        config_data = self.file_handler.read_yaml()
         if config_data:
             log.info("Config data exists", config_data=config_data)
             db_config = self.create_db_config(config_data)
-            log.info("db_config object created", object=db_config)
-            json_file = file_handler.convert_to_json(db_config)
-            file_handler.save_as_json(data=json_file,
-                                      file_name=os.path.splitext(os.path.basename(self.file_path))[0],
-                                      output_directory=self.destination)
-            log.info("File saved")
-            file_handler.save_as_json(data=json_file, file_name="db_config",
-                                      output_directory=self.destination)
-            log.info("File saved")
+            if db_config:
+                log.info("db_config object created", object=db_config)
+                self.file_handler.save_as_json(data=db_config.to_json(),
+                                               file_name=os.path.splitext(os.path.basename(self.file_path))[0])
+                log.info("File saved")
+
+                self.file_handler.save_as_json(data=db_config.to_json(), file_name="db_config")
+                log.info("File saved")
 
     def create_db_config(self, config_data):
         if self.validator.validate(config_data):
             log.info("Configuration is valid.", configuration=DbConfig(**config_data.get('config', {})))
             return DbConfig(**config_data.get('config', {}))
         else:
-            print("Invalid configuration:")
             for error in self.validator.errors:
-                print(f"  - {error}: {self.validator.errors[error]}")
                 log.warning("Invalid configuration:", f"  - {error}: {self.validator.errors[error]}")
             return None
