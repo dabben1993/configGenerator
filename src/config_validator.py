6cb71fc15685e3f5e1d5e61de1529a64439857d7
dabben1993 <gickis1993@live.com>
2024-01-30 14:39:26 +0100

    refactored config_validator.py & file_helper.py to make file_helper static

--- src/config_validator.py
+++ src/config_validator.py
@@ -2,8 +2,8 @@ import os
 from structlog import get_logger
 import cerberus
 import yaml
-from .db_config import DbConfig
-from .file_helper import FileHelper
+from db_config import DbConfig
+from src.file_helper import save_as_json, read_yaml
 
 log = get_logger()
 
@@ -16,7 +16,6 @@ class ConfigValidator:
         self.json_output_path = json_output_path
         self.destination = destination
         self.validator = self.load_schema()
-        self.file_handler = FileHelper(self.file_path, self.destination)
         self.process_configuration()
 
     def load_schema(self):
@@ -26,17 +25,18 @@ class ConfigValidator:
             return cerberus.Validator(schema)
 
     def process_configuration(self):
-        config_data = self.file_handler.read_yaml()
+        config_data = read_yaml(self.file_path)
         if config_data:
             log.info("Config data exists", config_data=config_data)
             db_config = self.create_db_config(config_data)
             if db_config:
                 log.info("db_config object created", object=db_config)
-                self.file_handler.save_as_json(data=db_config.to_json(),
-                                               file_name=os.path.splitext(os.path.basename(self.file_path))[0])
+                save_as_json(data=db_config.to_json(),
+                             file_name=os.path.splitext(os.path.basename(self.file_path))[0],
+                             output_directory=self.destination)
                 log.info("File saved")
 
-                self.file_handler.save_as_json(data=db_config.to_json(), file_name="db_config")
+                save_as_json(data=db_config.to_json(), file_name="db_config", output_directory=self.destination)
                 log.info("File saved")
 
     def create_db_config(self, config_data):
--- src/file_helper.py
+++ src/file_helper.py
@@ -6,26 +6,22 @@ from structlog import get_logger
 log = get_logger()
 
 
-class FileHelper:
-    def __init__(self, file_path, destination):
-        self.file_path = file_path
-        self.destination = destination
+def save_as_json(data, file_name, output_directory):
+    output_directory = output_directory
+    try:
+        os.makedirs(output_directory, exist_ok=True)
+        json_output_path = os.path.join(output_directory, file_name + ".json")
+        with open(json_output_path, 'w') as json_output_file:
+            json.dump(data, json_output_file, indent=2)
+        log.info("File saved as JSON", data=data, file_path=json_output_path)
+    except Exception as e:
+        log.error("Error saving JSON file", error=str(e))
 
-    def read_yaml(self):
-        try:
-            with open(self.file_path, 'r') as yaml_file:
-                return yaml.safe_load(yaml_file)
-        except Exception as e:
-            log.error("Error reading YAML file", error=str(e))
-            return None
 
-    def save_as_json(self, data, file_name, output_directory=None):
-        output_directory = output_directory or self.destination
-        try:
-            os.makedirs(output_directory, exist_ok=True)
-            json_output_path = os.path.join(output_directory, file_name + ".json")
-            with open(json_output_path, 'w') as json_output_file:
-                json.dump(data, json_output_file, indent=2)
-            log.info("File saved as JSON", data=data, file_path=json_output_path)
-        except Exception as e:
-            log.error("Error saving JSON file", error=str(e))
+def read_yaml(file_path):
+    try:
+        with open(file_path, 'r') as yaml_file:
+            return yaml.safe_load(yaml_file)
+    except Exception as e:
+        log.error("Error reading YAML file", error=str(e))
+        return None
