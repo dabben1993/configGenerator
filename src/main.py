9ed58a487694abad719455baba2a76e07ad5652d
dabben1993 <gickis1993@live.com>
2024-02-02 14:49:27 +0100

    access token not getting picked up?

--- src/main.py
+++ src/main.py
@@ -2,6 +2,7 @@ from config_validator import ConfigValidator
 from app_config import AppConfig
 import git_service
 import s3_service
+import structlog
 
 secrets = AppConfig()
 branch_name = "jenkins_test06"
@@ -9,34 +10,34 @@ git = git_service.GitService(pat=secrets.git_access_key)
 s3 = s3_service.S3Transfer(aws_access_key_id=secrets.aws_access_key_id,
                            aws_secret_access_key=secrets.aws_secret_access_key,
                            region_name="us-east-2")
-print("credentials: git=" + secrets.git_access_key)
-print("credentials: bb=" + secrets.bitbucket_access_token)
+log = structlog.get_logger()
+log.info("Credentials", git=secrets.git_access_key, bb=secrets.bitbucket_access_token)
 
-git.repo = git.clone_repo(repo_url=f"https://x-token-auth:{secrets.bitbucket_access_token}@bitbucket.org"
-                                   f"/config-generator/test.git",
-                          branch="main",
-                          destination="../repos/", secret=secrets.bitbucket_access_token)
+#git.repo = git.clone_repo(repo_url=f"https://x-token-auth:{secrets.bitbucket_access_token}@bitbucket.org"
+#                                   f"/config-generator/test.git",
+#                          branch="main",
+#                          destination="../repos/", secret=secrets.bitbucket_access_token)
 
 
 # Validate yml file and convert to JSON
-validator = ConfigValidator(repo_path=git.repo.working_dir,
-                            json_output_path=git.repo.working_dir + "/output/test.json",
-                            destination=git.repo.working_dir + "/output/",
-                            schema_path="../config/cerberus_schema.yml",
-                            file_name="test.yml")
+#validator = ConfigValidator(repo_path=git.repo.working_dir,
+#                            json_output_path=git.repo.working_dir + "/output/test.json",
+#                            destination=git.repo.working_dir + "/output/",
+#                            schema_path="../config/cerberus_schema.yml",
+#                            file_name="test.yml")
 
-git.list_remote_branches()
-
-
-git.create_and_push_to_new_branch(new_branch_name=branch_name,
-                                  commit_message="this is commit 215332",
-                                  bitbucket_access_key=secrets.bitbucket_access_token)
+#git.list_remote_branches()
 
 
-s3.upload_folder(local_folder_path=git.repo.working_dir + "/output/",
-                         bucket_name="timpabucket", s3_prefix="jenkins-test/")
-git.switch_branch("main")
-git.delete_local_branch(branch_name)
+#git.create_and_push_to_new_branch(new_branch_name=branch_name,
+#                                  commit_message="this is commit 215332",
+#                                  bitbucket_access_key=secrets.bitbucket_access_token)
+
+
+#s3.upload_folder(local_folder_path=git.repo.working_dir + "/output/",
+#                         bucket_name="timpabucket", s3_prefix="jenkins-test/")
+#git.switch_branch("main")
+#git.delete_local_branch(branch_name)
 
 # bitbucket_pull_request = BitbucketPullRequestHandler(username=secrets.bitbucket_username,
 #                                                     app_password=secrets.bitbucket_app_password)
